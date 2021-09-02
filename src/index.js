2b110dce706d16dc68359b91e922167ece250890
DEsimas <simasuh@gmail.com>
2021-09-02 19:44:43 +0300

    token changed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import Discord from "discord.js";
 const client = new Discord.Client();
-client.login("ODgzMDIwMTg2MzM5Mzk3Njkz.YTD2og.GuFhz5nib6a1kPwGrCDqC7YvsI0");
+client.login("ODgzMDIwMTg2MzM5Mzk3Njkz.YTD2og.wiUxcbVfzeJSwFqeRUMrcpzLNAU");
