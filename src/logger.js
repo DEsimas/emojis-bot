02de3c69ca02dabb78dc3ea1326bfc270d7db987
DEsimas <simasuh@gmail.com>
2021-09-29 23:47:10 +0300

    logger fix

--- src/logger.js
+++ src/logger.js
@@ -1,19 +1,22 @@
-import colors from "colors";
+const red = '\x1b[31m%s\x1b[0m';
+const green = '\x1b[32m%s\x1b[0m';
+const blue = '\x1b[34m%s\x1b[0m';
+const cyan = '\x1b[36m%s\x1b[0m';
 
 export function logError (text, data) {
     if(data)
-        console.log((new Date().toString()+":").gray, (text+"\n").red, data, "\n");
+        console.log(red, new Date().toString()+": " + text+"\n", data, "\n");
     else
-        console.log((new Date().toString()+":").gray, (text+"\n").red);
+        console.log(red, new Date().toString()+": " + text+"\n");
 };
 
 export function logWarning (text, data) {
     if(data)
-        console.log((new Date().toString()+":").gray, (text+"\n").brightYellow, data, "\n");
+        console.log(new Date().toString()+": " + text+"\n", data, "\n");
     else
-        console.log((new Date().toString()+":").gray, (text+"\n").brightYellow);
+        console.log(new Date().toString()+": " + text+"\n");
 };
 
 export function logInfo (text) {
-    console.log((new Date().toString()+":").gray, (text+"\n").green);
+    console.log(green, new Date().toString()+": " + text+"\n");
 };
