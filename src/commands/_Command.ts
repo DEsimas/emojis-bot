ed2c979cd897b4ae34d9cc21b430ff928e611049
DEsimas <simasuh@gmail.com>
2021-12-20 17:34:07 +0300

    commands handler added

---
+++ src/commands/_Command.ts
@@ -0,0 +1,55 @@
+import { Client, Message, MessageEmbed } from "discord.js";
+import { config } from "../config";
+import { Server } from "../database/Servers";
+import { User } from "../database/Users";
+
+export class Command {
+    protected readonly client: Client;
+    protected readonly message: Message;
+    protected readonly user: User;
+    protected readonly server: Server;
+    protected readonly args: string[];
+
+    constructor(client: Client, message: Message, user: User, server: Server, args: string[]) {
+        this.client = client;
+        this.message = message;
+        this.user = user;
+        this.server = server;
+        this.args = args;
+    }
+
+    public async execute(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    protected validateURL(str: string): boolean {
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        return !!pattern.test(str);
+    }
+
+    protected sendError(message: string): void {
+        const embed = new MessageEmbed()
+            .setColor(config.embed_colors.error)
+            .setTitle(message);
+        this.message.channel.send({ embeds: [embed] });
+    }
+
+    protected sendSuccess(message: string): void {
+        const embed = new MessageEmbed()
+            .setColor(config.embed_colors.success)
+            .setTitle(message);
+        this.message.channel.send({ embeds: [embed] });
+    }
+
+    protected sendRegular(message: string): void {
+        const embed = new MessageEmbed()
+            .setColor(config.embed_colors.discord)
+            .setTitle(message);
+        this.message.channel.send({ embeds: [embed] });
+    }
+};
--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -1,4 +1,6 @@
 import { Client, Message } from "discord.js";
+import { Command } from "../commands/_Command";
+import { config } from "../config";
 import { Server } from "../database/Servers";
 import { User } from "../database/Users";
 
@@ -7,15 +9,40 @@ export class CommandHandler {
     private readonly message: Message;
     private readonly user: User;
     private readonly server: Server;
+    private readonly commands: Array<{ name: string[], out: typeof Command }> = [];
 
     constructor(client: Client, message: Message, user: User, server: Server) {
         this.client = client;
         this.message = message;
         this.user = user;
         this.server = server;
+        this.setCommandsList(Command, Command, Command, Command, Command, Command, Command, Command, Command, Command);
     }
 
     public async handle(): Promise<void> {
-        console.log("handling command ....");
+        if (this.message.author.id !== this.client.user?.id) {
+            const content = this.message.content.trim();
+            const args = content.split(" ");
+            const command = args[0].toLowerCase();
+            
+            this.commands.forEach(cmd => {
+                cmd.name.forEach(name => {
+                    if (this.server.prefix + name === command)
+                        new cmd.out(this.client, this.message, this.user, this.server, args).execute();
+                });
+            });
+        };
+    }
+
+    private setCommandsList(...commands: Array<typeof Command>): void {
+        if(commands.length !== Object.keys(config.commands).length) throw new Error("CommandsHandler.ts: number of commands doesn't match the number of handlers");
+
+        let k: keyof typeof config.commands, i: number = 0;
+        for(k in config.commands){
+            this.commands.push({
+                name: config.commands[k],
+                out: commands[i++]
+            });
+        }
     }
 };
