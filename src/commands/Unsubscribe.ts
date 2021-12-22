c203b5cf821936957ed0158debac1dbb0205f150
DEsimas <simasuh@gmail.com>
2021-12-22 19:12:53 +0300

    unsubscribe

--- src/commands/Unsubscribe.ts
+++ src/commands/Unsubscribe.ts
@@ -1,7 +1,17 @@
+import { DAO } from "../database/DAO";
 import { Command } from "./_Command";
 
 export class Unsubscribe extends Command {
     public override async execute(): Promise<void> {
-        this.sendError("Method not implemented.");
+        const id = this.message.author.id;
+
+        if(await DAO.Notifications.findByUserId(id) === null) {
+            this.sendError(this.localization.unsubscribe.not_subbed);
+            return
+        }
+
+        DAO.Notifications.deleteByUserId(id);
+
+        this.sendSuccess(this.localization.unsubscribe.success);
     }
 }
