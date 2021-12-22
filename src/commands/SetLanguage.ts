68a8b1df9cceef905c873c92ada5e71d21d89c7c
DEsimas <simasuh@gmail.com>
2021-12-22 22:00:41 +0300

    setlanguage

--- src/commands/SetLanguage.ts
+++ src/commands/SetLanguage.ts
@@ -1,7 +1,36 @@
+import { Localization } from "../config/Localization";
+import { DAO } from "../database/DAO";
 import { Command } from "./_Command";
 
 export class SetLanguage extends Command {
     public override async execute(): Promise<void> {
-        this.sendError("Method not implemented.");
+        const language = this.getLanguage();
+        const userID = this.message.author.id;
+
+        if(!language) return;
+    
+        DAO.Users.updateByUserId(userID, { language: language })
+            .then(() => this.sendSuccess(this.localization.setLanguage.success + language))
+            .catch(() => super.sendError(this.localization.setLanguage.db_error));
     }
+
+    private getLanguage(): string | undefined {
+        const language = this.args[1];
+        if(language === undefined) {
+            this.sendError(this.localization.setLanguage.empty);
+            return undefined;
+        };
+
+        let flag = false;
+        Object.keys(Localization).map(objectKey => {
+            if (objectKey === language) flag = true;
+        });
+
+        if (!flag) {
+            this.sendError(this.localization.setLanguage.warn);
+            return undefined;
+        };
+
+        return language;
+    };
 }
