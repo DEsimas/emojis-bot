9b397bb4eeb2e498c0e3d1f21470d5c94bd2dd98
DEsimas <simasuh@gmail.com>
2021-12-22 21:42:07 +0300

    deleteemoji

--- src/commands/DeleteEmoji.ts
+++ src/commands/DeleteEmoji.ts
@@ -1,7 +1,10 @@
+import { DAO } from "../database/DAO";
 import { Command } from "./_Command";
 
 export class DeleteEmoji extends Command {
     public override async execute(): Promise<void> {
-        this.sendError("Method not implemented.");
+        DAO.Users.updateByUserId(this.message.author.id, { emojiID: null })
+            .then(() => this.sendSuccess(this.localization.deleteEmoji.success))
+            .catch(() => this.sendError(this.localization.deleteEmoji.db_error));
     }
 }
--- src/commands/SetPrefix.ts
+++ src/commands/SetPrefix.ts
@@ -6,12 +6,12 @@ export class SetPrefix extends Command {
         if(!await this.isAdmin()) {
             this.sendError(this.localization.setPrefix.access_warn);
             return;
-        };
+        }
 
         if (this.args[1] === undefined) {
             this.sendError(this.localization.setPrefix.empty_warn);
             return;
-        };
+        }
 
         DAO.Servers.updateOneByServerId(this.server.serverID, { prefix: this.args[1] })
             .then(() => this.sendSuccess(this.localization.setPrefix.success + this.args[1]))
