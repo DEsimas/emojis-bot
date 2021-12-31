10b3f69a49a83e5634226954c709b4a65b7ef1fb
DEsimas <simasuh@gmail.com>
2021-12-31 17:54:04 +0300

    Revert "Bot class refactoring"
    This reverts commit 38e6d20b97146f02eeade10cecae3bacda00537a.

--- src/handlers/readyHandler.ts
+++ src/handlers/readyHandler.ts
@@ -1,17 +1,23 @@
-import { UserInterfaceUpdater } from "../components/UserInterfaceUpdater";
-import { NotificationsSender } from "../components/NotificationsSender";
-import { Log } from "../Log";
+import { UserInterfaceUpdater } from "./../components/UserInterfaceUpdater";
+import { NotificationsSender } from "./../components/NotificationsSender";
+import { Log } from "./../Log";
 
-import { EventHandler } from "./_EventHandler";
+import { Client } from "discord.js";
 
-export class ReadyHandler extends EventHandler {
+export class ReadyHandler {
+    private readonly client: Client;
+
     private readonly cron: Record<string, string> = {
         notifications: "0 0 * * *",
         UI: "*/5 * * * *"
     }
-    
-    public override async handle(): Promise<void> {
+
+    constructor(client: Client)  {
         Log.info("ReadyHandler.ts", "bot is ready");
+        this.client = client;
+    }
+
+    public async handle(): Promise<void> {
         const sender = new NotificationsSender(this.client);
         sender.getTask(this.cron.notifications).start();
 
