16012e483ce56e614b8d5b23196b5e7977f3be46
DEsimas <simasuh@gmail.com>
2022-01-05 17:59:32 +0300

    check if arg is category implemented

--- src/commands/interaction/Help.ts
+++ src/commands/interaction/Help.ts
@@ -1,12 +1,22 @@
 import { MessageEmbed } from "discord.js";
 import { DAO } from "./../../database/DAO";
 import { Command } from "./../Command";
-import { categories } from "../../config/CommandsList";
+import { categories, commands } from "../../config/CommandsList";
 import { help } from "../../config/Localization";
+import { Categories } from "../../config/Types";
 
 export class Help extends Command {
     public override async execute(): Promise<void> {
-        this.sendCategories();
+        const category = this.getCategory();
+        if(category) this.sendCommands(category);
+        else this.sendCategories();
+    }
+
+    private getCategory(): Categories | null {
+        for(let i = 0; i < categories.length; i++) {
+            if(categories[i] === this.args[1]?.toLowerCase()) return categories[i];
+        }
+        return null;
     }
 
     private async getEmbed(title: string): Promise<MessageEmbed> {
@@ -28,5 +38,9 @@ export class Help extends Command {
         });
 
         this.message.channel.send({ embeds: [embed] });
+    }
+
+    private async sendCommands(category: Categories): Promise<void> {
+        this.message.channel.send("uwu");
     }
 }
