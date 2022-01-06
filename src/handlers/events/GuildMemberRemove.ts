bcfda682fe3b7c0a587fde79999664d86dbe9b34
DEsimas <simasuh@gmail.com>
2022-01-06 18:57:33 +0300

    members handling

---
+++ src/handlers/events/GuildMemberRemove.ts
@@ -0,0 +1,26 @@
+import { GuildMember, MessageEmbed } from "discord.js";
+import { guildMembers } from "../../config/Localization";
+import { DAO } from "../../database/DAO";
+import { Handler } from "../Handler";
+
+export class GuildMemberRemove extends Handler {
+    private readonly member: GuildMember;
+
+    constructor(member: GuildMember) {
+        super();
+        this.member = member;
+    }
+
+    public async handle(): Promise<void> {
+        if(!(await DAO.Servers.fetchByServerId(this.member.guild.id)).doGreetings) return;
+
+        const embed = new MessageEmbed()
+            .setColor(this.embedColors.error)
+            .setThumbnail(this.member.user.avatarURL() || this.defaultAvatar)
+            .setTitle(guildMembers.bye + this.member.user.username)
+            .setFooter(this.member.guild.name)
+            .setTimestamp();
+        
+        this.member.guild.systemChannel?.send({ embeds: [embed] });
+    }
+}
