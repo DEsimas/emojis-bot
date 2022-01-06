44a2c711ad865a746de595389d1e355dfa29e412
DEsimas <simasuh@gmail.com>
2022-01-06 17:22:49 +0300

    getavatar command implemented

--- src/commands/interaction/GetAvatar.ts
+++ src/commands/interaction/GetAvatar.ts
@@ -1,7 +1,19 @@
+import { MessageEmbed } from "discord.js";
+import { DAO } from "../../database/DAO";
 import { Command } from "../Command";
 
 export class GetAvatar extends Command {
     public override async execute(): Promise<void> {
-        console.log("uwu");
+        const user = this.message.mentions.users.first();
+        if (!user) return this.sendError(this.localization.error);
+        const avatarURL = user.avatarURL();
+        if (!avatarURL) return this.sendError(this.localization.error);
+
+        const embedColor = (await DAO.Avatars.getActive())?.color;
+        const embed = new MessageEmbed()
+            .setColor(embedColor || this.embedColors.discord)
+            .setImage(avatarURL);
+
+        this.message.channel.send({ embeds: [embed] });
     }
 }
