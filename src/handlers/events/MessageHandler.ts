d000ea4802f85f53e4cf468f4cd960465f35ae01
DEsimas <simasuh@gmail.com>
2022-02-06 18:56:10 +0300

    handling errors during reacting messages

--- src/handlers/events/MessageHandler.ts
+++ src/handlers/events/MessageHandler.ts
@@ -1,6 +1,7 @@
 import { CommandHandler } from "./../other/CommandHandler";
+import { Log } from "./../../components/Log";
 import { DAO } from "./../../database/DAO";
-import { Handler } from "../Handler";
+import { Handler } from "./../Handler";
 
 import { Client, Message } from "discord.js";
 
@@ -13,20 +14,28 @@ export class MessageHandler extends Handler {
         this.client = client;
         this.message = message;
     }
-    
+
     public async handle(): Promise<void> {
-        if(!this.message.guild?.id) return; // stops handling if message not from a server
+        if (!this.message.guild?.id) return; // stops handling if message not from a server
 
         const user = await DAO.Users.fetchByUserId(this.message.author.id);
         const server = await DAO.Servers.fetchByServerId(this.message.guild.id);
 
-        if(server.doEmojis) {
-            if(this.message.author.id !== this.client.user?.id && user.emojiID) {
-                this.message.react(user.emojiID);
-            } else if(this.message.author.id === this.client.user?.id) {
+        if (server.doEmojis) {
+            if (this.message.author.id !== this.client.user?.id && user.emojiID) {
+                try {
+                    await this.message.react(user.emojiID);
+                } catch {
+                    Log.warning("MessageHandler.ts", "Failed to react users message", { user: user, server: server });
+                }
+            } else if (this.message.author.id === this.client.user?.id) {
                 const avatar = await DAO.Avatars.getActive();
-                if(avatar) {
-                    this.message.react(avatar.emojiID);
+                if (avatar) {
+                    try {
+                        await this.message.react(avatar.emojiID);
+                    } catch {
+                        Log.warning("MessageHandler.ts", "Failed to react bot message", { avatar: avatar, server: server });
+                    }
                 }
             }
         }
