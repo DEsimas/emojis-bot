375398ae208f532dcdc6816fe89f99958b0505e8
DEsimas <simasuh@gmail.com>
2022-05-16 15:54:02 +0300

    Days counting function fix

--- src/components/StateExamCounter.ts
+++ src/components/StateExamCounter.ts
@@ -44,6 +44,6 @@ export class StateExamCounter {
     private getDifferenceInDays(begin: Date, end: Date): number | null {
         if(begin >= end) return null;
         const diff = end.getTime() - begin.getTime();
-        return Math.floor( diff / (1000 * 60 * 60 * 24)); 
+        return Math.floor( diff / (1000 * 60 * 60 * 24)) + 1; 
     }
 }
