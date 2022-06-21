c4cf5a010707c667776e9c6a4873d0f2b2b1f6e4
Serafim Sukharev <simasuh@gmail.com>
2022-06-21 17:13:42 +0300

    export of the bot class

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { config } from 'dotenv';
 import { Bot } from './Bot';
 
+export { Bot };
+
 config();
 
-if(!process.env.TOKEN) throw new Error("Token not found");
-if(!process.env.MONGO) throw new Error("Mongo uri not found");
-
-const bot = new Bot({token: process.env.TOKEN, mongo_uri: process.env.MONGO});
-bot.start();
+if(process.env.MODE == "BOT") {   
+    if(!process.env.TOKEN) throw new Error("Token not found");
+    if(!process.env.MONGO) throw new Error("Mongo uri not found");
+    
+    const bot = new Bot({token: process.env.TOKEN, mongo_uri: process.env.MONGO});
+    bot.start();
+}
