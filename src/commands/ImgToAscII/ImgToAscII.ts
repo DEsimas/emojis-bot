1c01d6d37988b649a08f5ba0c8fb524e8a03ad91
Serafim Sukharev <simasuh@gmail.com>
2022-08-07 01:22:04 +0300

    import axios

--- src/commands/ImgToAscII/ImgToAscII.ts
+++ src/commands/ImgToAscII/ImgToAscII.ts
@@ -1,17 +1,20 @@
 import { Log } from "../../components/Log";
 import { Command } from "./../Command";
+import axios from "axios";
 
 export class ImgToAscII extends Command {
+    private readonly converterURL = "http://api.foxtools.ru/v2/ImageToText";
+
     private readonly sizeParam = "size:";
     private readonly charsParam = "chars:";
 
-    private options: { chars: string, size: number } = { 
+    private options: { chars: string, size: number } = {
         chars: "##@%=+*:-. ",
-        size: 100,
+        size: 500,
     }
 
     public async execute(): Promise<void> {
-        if(this.args.length === 0) {
+        if (this.args.length === 0) {
             this.sendError(this.localization.no_files)
             return;
         }
@@ -19,14 +22,14 @@ export class ImgToAscII extends Command {
         const files = this.parseParams();
 
         files.forEach(async el => {
-            if(!this.validateURL(el)) {
+            if (!this.validateURL(el)) {
                 this.sendError(el + this.localization.not_link);
                 return;
             }
 
             const asciified = "uwu";
 
-            if(typeof asciified === "string") {    
+            if (typeof asciified === "string") {
                 let buf = Buffer.from(asciified);
                 this.message.channel.send({ files: [{ attachment: buf, name: "ascII.txt" }] }).catch(error => Log.error("ImgToAscII.ts", "Error while sending asciified file", { text: asciified, buffer: buf, error: error }));
             }
@@ -37,19 +40,19 @@ export class ImgToAscII extends Command {
         const files: string[] = [];
 
         this.args.forEach((el, index) => {
-            if(el.includes(this.sizeParam)) {
+            if (el.includes(this.sizeParam)) {
                 const size = Number(el.slice(this.sizeParam.length));
-                if(!isNaN(size))
+                if (!isNaN(size))
                     this.options.size = size;
-            } else if(el.includes(this.charsParam)) {
+            } else if (el.includes(this.charsParam)) {
                 const chars = el.slice(this.charsParam.length);
                 this.options.chars = chars.replace("[space]", " ");
-            } else if(index !== 0) {
+            } else if (index !== 0) {
                 files.push(el);
             }
         });
         this.message.attachments.forEach(el => files.push(el.url));
-        
+
         return files;
     }
 }
