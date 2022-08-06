a1335f45d59aeb5534963eea972a1464036c930f
Serafim Sukharev <simasuh@gmail.com>
2022-08-06 15:12:31 +0300

    disable db logging

--- src/components/Log.ts
+++ src/components/Log.ts
@@ -1,40 +1,44 @@
 import { DAO } from "./../database/DAO";
 
 export class Log {
+    private static readonly doDBLogging = false;
     private static readonly red = '\x1b[31m%s\x1b[0m';
     private static readonly yellow = '\x1b[33m%s\x1b[0m';
     private static readonly blue = '\x1b[34m%s\x1b[0m';
 
-    static error (origin: string, message: string, data?: any): void {
+    static error(origin: string, message: string, data?: any): void {
         console.log(this.red, origin + " -> " + message);
-        DAO.Logs.insertOne({
-            type: "error",
-            time: new Date(),
-            origin: origin,
-            message: message,
-            payload: JSON.stringify(data)
-        });
+        if (this.doDBLogging)
+            DAO.Logs.insertOne({
+                type: "error",
+                time: new Date(),
+                origin: origin,
+                message: message,
+                payload: JSON.stringify(data)
+            });
     };
-    
-    static warning (origin: string, message: string, data?: any): void {
+
+    static warning(origin: string, message: string, data?: any): void {
         console.log(this.yellow, origin + " -> " + message);
-        DAO.Logs.insertOne({
-            type: "warning",
-            time: new Date(),
-            origin: origin,
-            message: message,
-            payload: JSON.stringify(data)
-        });
+        if (this.doDBLogging)
+            DAO.Logs.insertOne({
+                type: "warning",
+                time: new Date(),
+                origin: origin,
+                message: message,
+                payload: JSON.stringify(data)
+            });
     };
-    
-    static info (origin: string, message: string, data?: any): void {
+
+    static info(origin: string, message: string, data?: any): void {
         console.log(this.blue, origin + " -> " + message);
-        DAO.Logs.insertOne({
-            type: "info",
-            time: new Date(),
-            origin: origin,
-            message: message,
-            payload: JSON.stringify(data)
-        });
+        if (this.doDBLogging)
+            DAO.Logs.insertOne({
+                type: "info",
+                time: new Date(),
+                origin: origin,
+                message: message,
+                payload: JSON.stringify(data)
+            });
     };
 };
